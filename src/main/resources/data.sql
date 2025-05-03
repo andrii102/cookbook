74be9d70c2a5103222c442aebbf97e53b1750f20
AndriiKohut <andriykohyt2017@gmail.com>
2025-05-03 15:52:26 +0300

    Fixed data.sql and forms

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -62,104 +62,103 @@ INSERT INTO Dish (name, type, description, total_calories, total_weight) VALUES
          ('Caprese Salad', 'SALAD', 'Fresh mozzarella and tomatoes', 320, 250),
          ('Garlic Shrimp Pasta', 'MAIN', 'Garlicky shrimp with pasta', 450, 300);
 
--- Assign ingredients to dishes
+-- Greek Salad
 INSERT INTO DishIngredient (dish_id, ingredient_id, weight)
-SELECT d.id, i.id, w.weight
-FROM Dish d
-         JOIN Ingredient i ON i.name = w.ingredient_name
-         JOIN (VALUES
-               -- Greek Salad
-               ('Tomato', 100),
-               ('Cucumber', 50),
-               ('Red Onion', 30),
-               ('Feta', 50),
-               ('Olive Oil', 15),
-               ('Salt', 2),
-               ('Black Pepper', 1),
+SELECT d.id, i.id, w.weight FROM Dish d JOIN Ingredient i ON i.name = w.ingredient_name
+    JOIN (VALUES
+              ('Tomato', 100),
+              ('Cucumber', 50),
+              ('Red Onion', 30),
+              ('Feta', 50),
+              ('Olive Oil', 15),
+              ('Salt', 2),
+              ('Black Pepper', 1)
+) AS w(ingredient_name, weight) WHERE d.name = 'Greek Salad' AND i.name = w.ingredient_name;
 
-               -- Chicken Soup
-               ('Chicken Fillet', 150),
-               ('Carrot', 50),
-               ('Celery', 30),
-               ('Salt', 3),
-               ('Black Pepper', 2),
+-- Chicken Soup
+INSERT INTO DishIngredient (dish_id, ingredient_id, weight)
+SELECT d.id, i.id, w.weight FROM Dish d JOIN Ingredient i ON i.name = w.ingredient_name
+    JOIN (VALUES
+              ('Chicken Fillet', 150),
+              ('Carrot', 50),
+              ('Celery', 30),
+              ('Salt', 3),
+              ('Black Pepper', 2)
+) AS w(ingredient_name, weight) WHERE d.name = 'Chicken Soup' AND i.name = w.ingredient_name;
 
-               -- Spaghetti Carbonara
-               ('Spaghetti', 200),
-               ('Pancetta', 100),
-               ('Egg', 100),
-               ('Parmesan', 50),
-               ('Black Pepper', 3),
+-- Spaghetti Carbonara
+INSERT INTO DishIngredient (dish_id, ingredient_id, weight)
+SELECT d.id, i.id, w.weight FROM Dish d JOIN Ingredient i ON i.name = w.ingredient_name
+    JOIN (VALUES
+              ('Spaghetti', 200),
+              ('Pancetta', 100),
+              ('Egg', 100),
+              ('Parmesan', 50),
+              ('Black Pepper', 3)
+) AS w(ingredient_name, weight) WHERE d.name = 'Spaghetti Carbonara' AND i.name = w.ingredient_name;
 
-               -- Margherita Pizza
-               ('Tomato', 150),
-               ('Mozzarella', 200),
-               ('Basil', 10),
-               ('Olive Oil', 10),
-               ('Salt', 2),
-
-               -- Chocolate Lava Cake
-               ('Dark Chocolate', 100),
-               ('Butter', 50),
-               ('Egg', 100),
-               ('Sugar', 50),
-               ('Flour', 30)
-) AS w(ingredient_name, weight)
-WHERE d.name = 'Greek Salad' AND i.name = w.ingredient_name;
-
--- For other dishes, repeat the pattern:
 -- Minestrone Soup
 INSERT INTO DishIngredient (dish_id, ingredient_id, weight)
-SELECT d.id, i.id, w.weight
-FROM Dish d
-         JOIN Ingredient i ON i.name = w.ingredient_name
-         JOIN (VALUES
-                   ('Tomato', 100),
-                   ('Carrot', 50),
-                   ('Celery', 30),
-                   ('Penne', 50),
-                   ('Olive Oil', 10),
-                   ('Salt', 3)
-) AS w(ingredient_name, weight)
-WHERE d.name = 'Minestrone Soup' AND i.name = w.ingredient_name;
+SELECT d.id, i.id, w.weight FROM Dish d JOIN Ingredient i ON i.name = w.ingredient_name
+    JOIN (VALUES
+              ('Tomato', 100),
+              ('Carrot', 50),
+              ('Celery', 30),
+              ('Penne', 50),
+              ('Olive Oil', 10),
+              ('Salt', 3)
+) AS w(ingredient_name, weight) WHERE d.name = 'Minestrone Soup' AND i.name = w.ingredient_name;
 
 -- Caesar Salad
 INSERT INTO DishIngredient (dish_id, ingredient_id, weight)
-SELECT d.id, i.id, w.weight
-FROM Dish d
-         JOIN Ingredient i ON i.name = w.ingredient_name
-         JOIN (VALUES
-                   ('Iceberg Lettuce', 150),
-                   ('Parmesan', 30),
-                   ('Breadcrumbs', 20),
-                   ('Olive Oil', 20),
-                   ('Salt', 2)
-) AS w(ingredient_name, weight)
-WHERE d.name = 'Caesar Salad' AND i.name = w.ingredient_name;
+SELECT d.id, i.id, w.weight FROM Dish d JOIN Ingredient i ON i.name = w.ingredient_name
+    JOIN (VALUES
+              ('Iceberg Lettuce', 150),
+              ('Parmesan', 30),
+              ('Breadcrumbs', 20),
+              ('Olive Oil', 20),
+              ('Salt', 2)
+) AS w(ingredient_name, weight) WHERE d.name = 'Caesar Salad' AND i.name = w.ingredient_name;
+
+-- Margherita Pizza
+INSERT INTO DishIngredient (dish_id, ingredient_id, weight)
+SELECT d.id, i.id, w.weight FROM Dish d JOIN Ingredient i ON i.name = w.ingredient_name
+    JOIN (VALUES
+              ('Tomato', 150),
+              ('Mozzarella', 200),
+              ('Basil', 10),
+              ('Olive Oil', 10),
+              ('Salt', 2)
+) AS w(ingredient_name, weight) WHERE d.name = 'Margherita Pizza' AND i.name = w.ingredient_name;
+
+-- Chocolate Lava Cake
+INSERT INTO DishIngredient (dish_id, ingredient_id, weight)
+SELECT d.id, i.id, w.weight FROM Dish d JOIN Ingredient i ON i.name = w.ingredient_name
+    JOIN (VALUES
+              ('Dark Chocolate', 100),
+              ('Butter', 50),
+              ('Egg', 100),
+              ('Sugar', 50),
+              ('Flour', 30)
+) AS w(ingredient_name, weight) WHERE d.name = 'Chocolate Lava Cake' AND i.name = w.ingredient_name;
 
 -- Caprese Salad
 INSERT INTO DishIngredient (dish_id, ingredient_id, weight)
-SELECT d.id, i.id, w.weight
-FROM Dish d
-         JOIN Ingredient i ON i.name = w.ingredient_name
-         JOIN (VALUES
-                   ('Tomato', 120),
-                   ('Mozzarella', 120),
-                   ('Basil', 5),
-                   ('Olive Oil', 10)
-) AS w(ingredient_name, weight)
-WHERE d.name = 'Caprese Salad' AND i.name = w.ingredient_name;
+SELECT d.id, i.id, w.weight FROM Dish d JOIN Ingredient i ON i.name = w.ingredient_name
+    JOIN (VALUES
+              ('Tomato', 120),
+              ('Mozzarella', 120),
+              ('Basil', 5),
+              ('Olive Oil', 10)
+) AS w(ingredient_name, weight) WHERE d.name = 'Caprese Salad' AND i.name = w.ingredient_name;
 
 -- Garlic Shrimp Pasta
 INSERT INTO DishIngredient (dish_id, ingredient_id, weight)
-SELECT d.id, i.id, w.weight
-FROM Dish d
-         JOIN Ingredient i ON i.name = w.ingredient_name
-         JOIN (VALUES
-                   ('Shrimp', 150),
-                   ('Spaghetti', 200),
-                   ('Garlic', 20),
-                   ('Olive Oil', 30),
-                   ('Salt', 3)
-) AS w(ingredient_name, weight)
-WHERE d.name = 'Garlic Shrimp Pasta' AND i.name = w.ingredient_name;
+SELECT d.id, i.id, w.weight FROM Dish d JOIN Ingredient i ON i.name = w.ingredient_name
+    JOIN (VALUES
+              ('Shrimp', 150),
+              ('Spaghetti', 200),
+              ('Garlic', 20),
+              ('Olive Oil', 30),
+              ('Salt', 3)
+) AS w(ingredient_name, weight) WHERE d.name = 'Garlic Shrimp Pasta' AND i.name = w.ingredient_name;
